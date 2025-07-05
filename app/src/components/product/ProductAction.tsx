45e572798a4602865ed3489a2589a3f4775cae38
Ionut Velicu <velicu.ionut@gmail.com>
2025-07-05 15:38:02 +0300

    fix

--- app/src/components/product/ProductAction.tsx
+++ app/src/components/product/ProductAction.tsx
@@ -1,14 +1,20 @@
 import { useEffect, useState } from "react";
 import ProductApi from "../../api/ProductApi";
-import { Button } from "antd";
+import { Button, Modal } from "antd";
 
 interface ProductActionProps {
   id: string;
   price: number;
 }
 
+//todo add stripe widget first time add cart details, option to save payment for later, subsequent buys can use existing payment
+// save on the server - create new endpoint /product/{courseId}/buy in ProductResource - entry is saved in MemberProduct
+// after you create the resource and dtos on the backedn you can go into /app folder and run "npm run gen api" to generate frontend dtos and axios requests 
+// you can then call them in ProductAPI
+
 export default function ProductAction({ id, price }: ProductActionProps) {
   const [loading, setLoading] = useState(true);
+  const [isBuyModal, setIsBuyModal] = useState(false);
   const [purchased, setPurchased] = useState(false);
 
   useEffect(() => {
@@ -19,10 +25,6 @@ export default function ProductAction({ id, price }: ProductActionProps) {
     });
   }, [id]);
 
-  function goToBuy() {
-    //TODO Teo
-  }
-
   return <>
     {!loading && <>
       {purchased && <Button>
@@ -30,9 +32,13 @@ export default function ProductAction({ id, price }: ProductActionProps) {
         Go to course
       </Button>}
 
-      {!purchased && <Button onClick={goToBuy}>
+      {!purchased && <Button onClick={() => setIsBuyModal(true)}>
         ${price} Buy now
       </Button>}
+
+      {isBuyModal && <Modal>
+        
+        </Modal>}
     </>}
   </>
 }
