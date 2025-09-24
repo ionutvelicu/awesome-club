6d3cfe610945da76dae16a7e1480bb4bf56dacb0
Ionut Velicu <velicu.ionut@gmail.com>
2025-09-24 12:18:40 +0300

    astro update

--- app/src/util/routes.ts
+++ app/src/util/routes.ts
@@ -2,5 +2,5 @@ export const Routes = {
   Root: "/",
   Dashboard: "/app/dashboard",
   Courses: "/app/courses",
-  ProductsNew: "/dashboard/products/new",
+  ProductsNew: "/app/dashboard/products/new",
 };
