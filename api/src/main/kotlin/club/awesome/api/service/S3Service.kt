b303c1501f01d30845d72977fa865d1e543d0cac
Alin Ghinea <alinghinea98@yahoo.com>
2025-09-25 17:24:51 +0300

    fix aws credentials on request

--- api/src/main/kotlin/club/awesome/api/service/S3Service.kt
+++ api/src/main/kotlin/club/awesome/api/service/S3Service.kt
@@ -1,27 +1,31 @@
 package club.awesome.api.service
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.s3.S3Client
 import software.amazon.awssdk.services.s3.model.PutObjectRequest
 import java.nio.file.Path
 
 @Service
-class S3Service {
+class S3Service(
+    @Value("\${aws.access-key}") private val accessKey: String,
+    @Value("\${aws.secret-key}") private val secretKey: String,
+    @Value("\${aws.region}") private val region: String,
+    @Value("\${aws.s3.bucket-name}") private val bucketName: String
+) {
 
-    private val bucketName = "my-awesome-bucket"
     private val s3: S3Client = S3Client.builder()
-        .region(Region.US_EAST_1) // set region as needed
-        .credentialsProvider(DefaultCredentialsProvider.create())
+        .region(Region.of(region))
+        .credentialsProvider(
+            StaticCredentialsProvider.create(
+                AwsBasicCredentials.create(accessKey, secretKey)
+            )
+        )
         .build()
 
-    /**
-     * Upload file to S3
-     * @param path - local path to the file
-     * @param key - S3 key (e.g., "product/123/section-1/original.mp4")
-     * @param contentType - mime type
-     */
     fun uploadFile(path: Path, key: String, contentType: String?) {
         val request = PutObjectRequest.builder()
             .bucket(bucketName)
